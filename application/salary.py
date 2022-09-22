54ba978fdfc86df07e8840ec279936036351fd56
Александра Деминова <moymail.09@yandex.ru>
2022-09-22 21:55:28 +0300

    Initial commit

---
+++ application/salary.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+
+def calculate_salary():
+    print(datetime.today())
---
+++ main.py
@@ -0,0 +1,7 @@
+from application.salary import calculate_salary as cs
+from application.db.people import get_employees as ge
+
+
+if __name__ == '__main__':
+    cs()
+    ge()
